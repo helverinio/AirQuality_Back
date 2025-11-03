c0be803051462abf8e76d2f17cf863bf2c9e323d
luzgomez <luzangelagomezg@gmail.com>
2025-11-02 21:56:41 -0500

    feat: Enhance pollutant creation with validation, error handling, and logging

--- src/pollutants/create-pollutant.dto.ts
+++ src/pollutants/create-pollutant.dto.ts
@@ -1,4 +1,11 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class CreatePollutantDto {
+  @IsString()
+  @IsNotEmpty({ message: 'Description is required' })
   description: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'Unit of measure is required' })
   unitOfMeasure: string;
 }
